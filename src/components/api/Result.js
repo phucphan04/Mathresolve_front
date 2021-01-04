d5076b362a5cd8653c724f2b1acb878cd8d3bdcb
Trình Trần <trinchuongtran@gmail.com>
2021-01-04 15:15:46 +0700

    oldcode

--- src/components/api/Result.js
+++ src/components/api/Result.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { Col, FormCheckbox } from "shards-react";
 import MathJax from 'react-mathjax2'
+
+import JXGBoard from 'jsxgraph-react-js'
+
 function Results({ tex }) {
     var [result, setResult] = useState("");
-    fetch("http://127.0.0.1:8000/api/backend_handing", {
+    fetch("http://127.0.0.1:8000/api/backend_handling", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -18,6 +21,10 @@ function Results({ tex }) {
         console.log(err);
         console.log("test1");
     })
+    let logicJS = (brd) => {
+        var c = brd.create('functiongraph', [function(x){ return x+1;}, -4, 4], { strokeColor: '#000000', strokeWidth: 3 });
+        brd.unsuspendUpdate();
+      }
     return (
         <Col sm="12" md="4" className="mb-3">
             <MathJax.Context input='tex'
@@ -35,6 +42,20 @@ function Results({ tex }) {
                     <MathJax.Node>{result}</MathJax.Node>
                 </div>
             </MathJax.Context>
+            <JXGBoard
+          logic={logicJS}
+          boardAttributes={{
+              axis: true,
+              boundingbox: [-12, 10, 12, -10],
+              showCopyright: false,
+              showReload: false,
+              showZoom: false,
+              showNavigation: false,
+            }}
+        //   style={{
+        //     border: "3px solid red"
+        //   }}
+        />
         </Col>
     )
 };
